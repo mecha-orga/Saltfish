af5944368bd7a35b0d0ab727787720a75563c890
bumfo <bumfo@users.noreply.github.com>
2017-01-06 01:49:38 +0800

    Enable restricted_user

--- tester/runcode.sh
+++ tester/runcode.sh
@@ -36,29 +36,30 @@ fi
 
 
 # Imposing memory limit with ulimit
-if [ "$EXT" != "java" ]; then
-	ulimit -v $((MEMLIMIT+10000))
-	ulimit -m $((MEMLIMIT+10000))
-	ulimit -s $((MEMLIMIT+10000))
-fi
+# if [ "$EXT" != "java" ]; then
+#   ulimit -v $((MEMLIMIT+10000))
+#   ulimit -m $((MEMLIMIT+10000))
+#   ulimit -s $((MEMLIMIT+10000))
+# fi
+# todo: figure out why this disables sudo
 
 # Imposing time limit with ulimit
 ulimit -t $TIMELIMITINT
 
-if $TIMEOUT_EXISTS; then
-	# Run the command with REAL time limit of TIMELIMITINT*2
-	timeout -s9 $((TIMELIMITINT*2)) $CMD <$IN >out 2>err
-else
-	# Run the command
-	$CMD <$IN >out 2>err	
-fi
+# if $TIMEOUT_EXISTS; then
+# 	# Run the command with REAL time limit of TIMELIMITINT*2
+# 	timeout -s9 $((TIMELIMITINT*2)) $CMD <$IN >out 2>err
+# else
+# 	# Run the command
+# 	$CMD <$IN >out 2>err	
+# fi
 # You can run submitted codes as another user:
 #
-# if $TIMEOUT_EXISTS; then
-# 	sudo -u another_user timeout -s9 $((TIMELIMITINT*2)) $CMD <$IN >out 2>err
-# else
-# 	sudo -u another_user $CMD <$IN >out 2>err	
-# fi
+if $TIMEOUT_EXISTS; then
+	sudo -u restricted_user timeout -s9 $((TIMELIMITINT*2)) $CMD <$IN >out 2>err
+else
+	sudo -u restricted_user $CMD <$IN >out 2>err	
+fi
 #
 # But you should change your sudoers file and allow the user running PHP (e.g. www-data in Ubuntu+Apache) to su to another_user
 # e.g. In Ubuntu (Apache running under www-data), run visudo and add this line:
@@ -67,7 +68,7 @@ EC=$?
 
 # KILL all processes of another_user (A process may still be alive!)
 # If you are running codes as another_user, also uncomment this line:
-#sudo -u another_user pkill -9 -u another_user
+sudo -u restricted_user pkill -9 -u restricted_user
 
 # Return exitcode
 exit $EC
